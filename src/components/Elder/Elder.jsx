2853200daa028778bbfc8adee38f3b4be7bd0f7c
Koonosuke <22fi041@ms.dendai.ac.jp>
2024-08-31 16:04:05 +0900

    third

--- src/components/Elder/Elder.jsx
+++ src/components/Elder/Elder.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import React from "react";
 
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
@@ -19,7 +19,7 @@ function Elder() {
         workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
       >
         <Viewer
-          fileUrl="/03_1.【3番目】最終プレゼンテーション資料_team1 .pdf"
+          fileUrl={`${process.env.PUBLIC_URL}/03_1.【3番目】最終プレゼンテーション資料_team1 .pdf`}
           plugins={[defaultLayoutPluginInstance]}
         />
       </Worker>
--- src/components/SNSapp/SNSAPP.jsx
+++ src/components/SNSapp/SNSAPP.jsx
@@ -19,7 +19,7 @@ function SNSAPP() {
         workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
       >
         <Viewer
-          fileUrl="/SNS_NodeandReact.pdf"
+          fileUrl={`${process.env.PUBLIC_URL}/SNS_NodeandReact.pdf`}
           plugins={[defaultLayoutPluginInstance]}
         />
       </Worker>
